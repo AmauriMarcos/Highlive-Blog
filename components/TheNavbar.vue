ffba6017284bdc10414a514c4ac562c8135a0043
AmauriMarcos <amaurisantos.m@gmail.com>
2020-09-07 16:58:54 +0200

    add menu

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -12,8 +12,7 @@
 
          <div class="mobile-menu">
              <input type="checkbox"  id="name" name="name" class="mobile-menu__name">
-             <label for="name" class="mobile-menu__name">MENU</label>
-             <label for="name" class="mobile-menu__xis" v-if="!show">X</label>
+             <label for="name" class="mobile-menu__name"></label>
 
              <nav class="mobile-menu__nav">
                  <ul class="mobile-menu__nav-list" >      
@@ -76,18 +75,7 @@ export default {
             show: 'false'
         }
     },
-    mounted(){
-        const menu = document.querySelector(".mobile-menu__name");
-        const xis  = document.querySelector(".mobile-menu__xis");
 
-        menu.addEventListener("click", () =>{
-            this.show = true
-        })
-
-        xis.addEventListener("click", () =>{
-            this.show = false
-        })
-    }
 }
 </script>
 <style lang="scss">
@@ -100,25 +88,14 @@ export default {
         @include respond(phone){
             @include center;
             display: block;
-            border-top: 1px solid black;
-            border-bottom: 1px solid black;
             width: 90vw;
-            margin: 2rem 0 !important;
-            transition: all .8s ease-in;
-                   
-        }
-
-        &__xis{
-            position: absolute;
-            top: 1rem;
-            right: 2rem; 
-            z-index: 150;
-            font-family: 'Muli-Light', sans-serif;
+            margin: 1rem 0 !important;
+            transition: all .8s ease-in;                  
         }
 
         &__nav{
-            background-color: rgba(0, 126, 255,.9);
-            width: 55%;
+            background-color: rgba(0, 0, 0,.85);
+            width: 51%;
             position: fixed;
             top: 0;
             right: 0;
@@ -126,13 +103,12 @@ export default {
             z-index: 100;
             transform: translateX(20rem);
              transition: all .2s ease-in-out;
-          
-            
+           
 
             &-list{
                 list-style: none;
                 position: relative;
-                 margin-top: 5rem;
+                 margin-top: 10rem;
                 
             }
 
@@ -145,16 +121,38 @@ export default {
                 text-transform: uppercase;
                 font-size: 1rem;
                 font-weight: bold;
+               
+               & a{
+                    color: $white;
+               }
 
             }
         }
 
         &__name{
             @include center;
-            padding: .5rem 0;
-            margin: 0 !important;
-            font-family: 'Muli-Light', sans-serif;
-            letter-spacing: .4rem;
+            position: absolute;
+            top: 3.5rem;
+            right: 3rem;
+            z-index: 500;
+            height: 4px;
+            border: 3px;
+            width: 10%;
+            display: inline-block;
+            background-color: rgba(0, 126, 255,.7);
+
+            &::before{
+                content: '';
+                background-color: rgba(0, 126, 255,.7);
+                width: 85%;
+                height: 3px;
+                display: inline-block;   
+
+            }
+
+            &::after{
+
+            }
         }
     }
 
@@ -172,15 +170,19 @@ export default {
         position: relative;
         z-index: 5;
 
+        @include respond(phone){
+             color:rgba(99, 133, 167, 0.7) !important;
+            
+         }
 
     }
    .nuxt-link-exact-active{
         color:rgba(0, 126, 255,.7) !important;    
          font-weight: bold;
 
-         @include respond(phone){
+         /* @include respond(phone){
              color: $white !important;
-         }
+         } */
     }
 
     @font-face {
@@ -194,11 +196,16 @@ export default {
         @include center;
         flex-direction: column;
 
+        @include respond(phone){
+          
+           
+        }
+
         &__brand{
         display: flex;
         justify-content: center;
         align-items: center;
-    
+
 
         &--title{
             font-family: 'Miya Wayne';
@@ -210,7 +217,8 @@ export default {
             cursor: pointer;
 
             @include respond(phone){
-                font-size: 4rem;
+                font-size: 3rem;
+                transform: translate(-7rem, 1rem);
             }
         }
 
@@ -220,7 +228,7 @@ export default {
             position: relative;
 
              @include respond(phone){
-                font-size: 6rem;
+                font-size: 4.5rem;
             }
 
             &:before{
@@ -232,11 +240,11 @@ export default {
                 display: inline-block;
                 position: absolute;
                 top: .3rem;
-                left: -.5rem;
+                left: -.6rem;
 
                 @include respond(phone){
-                     height: 75px;
-                     width: 75px;
+                     height: 60px;
+                     width: 60px;
                  }
             }           
         }      
